64616c99717d34fc57f9a8de1922ea10465b4879
jfoard2 <89165277+jfoard2@users.noreply.github.com>
2022-04-27 16:04:30 -0400

    loading in train and test data

--- cnn_svhn.py
+++ cnn_svhn.py
@@ -1,10 +1,43 @@
+from tokenize import PlainToken
 import numpy as np
 import os
-import PIL
-from PIL import image
+#import PIL
+#from PIL import image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import tensorflow as tf
 
 # os.chdir("/home/users/mlove/MSBA/BZAN 554 Deep Learning/Projects/Project 3")
+
+#create training data list
+os.chdir('C:/Users/jamfo/Documents/Deep Learning/bzan554-cnn-svhn/train')
+len([name for name in os.listdir('.') if os.path.isfile(name)]) #how many files in folder
+os.getcwd()
+
+ims_train=[]
+
+for i in range(1,1000):
+    img=mpimg.imread(str(i)+'.png')
+    ims_train.append(img)
+
+plt.imshow(ims_train[10])
+plt.show()
+
+
+#create testing data list
+os.chdir('C:/Users/jamfo/Documents/Deep Learning/bzan554-cnn-svhn/test')
+len([name for name in os.listdir('.') if os.path.isfile(name)]) #how many files in folder
+os.getcwd()
+
+ims_test=[]
+
+for i in range(1,1000):
+    img=mpimg.imread(str(i)+'.png')
+    ims_test.append(img)
+
+plt.imshow(ims_test[2])
+plt.show()
+
 
 ######################
 ### Project Phases ###
