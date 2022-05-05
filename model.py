1fa45128618a42045349277c903ce20bf46cd8c7
jfoard2 <89165277+jfoard2@users.noreply.github.com>
2022-05-05 19:02:02 -0400

    ready to build nn

--- model.py
+++ model.py
@@ -10,22 +10,20 @@ import matplotlib.image as mpimg
 
 marisa_path_repo=''
 cole_path_repo=''
-jake_path_repo='C:/Users/jamfo/Documents/Deep Learning/bzan554-cnn-svhn' #path to local repo
+path_repo_jake='C:/Users/jamfo/Documents/Deep Learning/bzan554-cnn-svhn' #path to local repo
 
 train_images_path_jake='C:/Users/jamfo/Documents/Deep Learning/SVHN/train_images_grey'
 test_images_path_jake='C:/Users/jamfo/Documents/Deep Learning/SVHN/test_images_grey'
 
-repo_path=jake_path_repo
+repo_path=path_repo_jake
 train_images_path=train_images_path_jake
 test_images_path=test_images_path_jake
 
-
 os.chdir(repo_path)
-import dataframe
 
 #create y train and y test
-train_df=dataframe.df_train
-test_df=dataframe.df_test
+train_df=pd.read_csv('train_labels.csv')
+test_df=pd.read_csv('test_labels.csv')
 y_train = train_df[['label']].to_numpy()
 y_test = test_df[['label']].to_numpy()
 
@@ -35,13 +33,13 @@ os.chdir(train_images_path)
 
 X_train=[]
 
-for i in range(33401):
+for i in range(33402):
     img=mpimg.imread(str(i+1)+'.png')
     X_train.append(img)
 
-plt.imshow(X_train[33400])
+plt.imshow(X_train[33401])
 plt.show()
-y_train[33400]
+y_train[33401]
 
 
 #create X test
@@ -49,11 +47,11 @@ os.chdir(test_images_path)
 
 X_test=[]
 
-for i in range(13067):
+for i in range(13068):
     img=mpimg.imread(str(i+1)+'.png')
     X_test.append(img)
 
-plt.imshow(X_test[5000])
+plt.imshow(X_test[13067])
 plt.show()
-y_test[13065]
+y_test[13067]
 
